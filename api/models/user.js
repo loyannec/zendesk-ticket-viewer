54b652de273d336ef5bd3e1fa35b297add86b583
Loyanne Repolho <loyanne.cristine@gmail.com>
2020-11-15 09:51:11 +0000

    Add Logged In User Request

--- api/models/user.js
+++ api/models/user.js
@@ -1,9 +1,16 @@
 const JSONInitializable = require('./json-initializable')
 
-class User {
+class User extends JSONInitializable {
     identifier
     name
     email
+
+    constructor(identifier, name, email) {
+        super()
+        this.identifier = identifier
+        this.name = name
+        this.email = email
+    }
 
     static createFromJson(json) {
         return new User(json.identifier, json.name, json.email)
