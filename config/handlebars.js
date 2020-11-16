97870502e07503dd6c472cfc402600332f3d640f
Loyanne Repolho <loyanne.cristine@gmail.com>
2020-11-16 07:48:02 +0000

    WIP

--- config/handlebars.js
+++ config/handlebars.js
@@ -2,13 +2,21 @@ module.exports = (app) => {
     const exphbs  = require('express-handlebars')
     const handlebars = exphbs.create({                 // Module that permits to render a template.
         helpers: {
-            isDefined: (value) => value !== undefined        // Adding helper to verify if value is defined.
+            formatDate: (date) => {
+                var time = date.toLocateTimeString([], {
+                    hour: '2-digit', minute: '2-digit'
+                })
+                var day = date.toLocateTimeString('en-IE', {
+                    weekday: 'long', year: 'numeric', month: 'long', day: 'numeric'
+                })
+                return `${time}, ${day}`
+            }
         }
-    });
+    })
 
     /*
     View engine setup
     */
     app.engine('handlebars', handlebars.engine)
     app.set('view engine', 'handlebars')
-};
+}
